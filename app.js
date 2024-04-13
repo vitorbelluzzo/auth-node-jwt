e29d9f6a3a9c21093665ced0d2b6cd5fc037244f
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-04-13 14:53:52 -0300

    feat: add root endpoint returning welcome message

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+import express, { request, response } from "express";
 import Mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const app = express()
+app.use(express.json())
+
+app.listen(3000)
+
+app.get('/',(request, response) => {
+  response.status(200).json({msg:'Bem vindo a nossa api!'})
+})
