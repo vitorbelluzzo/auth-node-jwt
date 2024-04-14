86213b7c5555a94ffb23f879bdc6be2e61ccd873
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-04-14 14:07:25 -0300

    feat: add MongoDB connection using environment variables

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { request, response } from "express";
-import Mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
 
 const app = express()
 app.use(express.json())
 
-app.listen(3000)
-
 app.get('/',(request, response) => {
   response.status(200).json({msg:'Bem vindo a nossa api!'})
 })
+
+const dbUser = process.env.dbUser
+const dbPass = process.env.dbPass
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.lcuvo3i.mongodb.net/`).then(() => {
+  app.listen(3000)
+  console.log("conectado");
+}).catch(((error) => 
+{ console.log(error)
+}))
